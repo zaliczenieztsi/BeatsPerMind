c1972f289df20cd672ba8dc1e2e6e14426c86ea9
Natalia <natalia.pasowe@gmail.com>
2026-05-03 20:43:04 +0200

    vercel improvement

--- app/vite.config.js
+++ app/vite.config.js
@@ -13,4 +13,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: path.resolve(__dirname, './dist'),
+    emptyOutDir: true,
+  },
 })
